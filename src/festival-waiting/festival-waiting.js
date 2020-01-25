b759bbd2e73597f9aecbaa1cf3f1b9bf4868bbe1
Stephen Wade <stephen@stephenwade.me>
2020-01-25 10:52:45 -0500

    festival-waiting displays start time

--- src/festival-waiting/festival-waiting.js
+++ src/festival-waiting/festival-waiting.js
@@ -1,4 +1,6 @@
 import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
+import '@polymer/paper-button/paper-button.js';
+import moment from 'moment/src/moment.js';
 
 /**
  * @customElement
@@ -10,10 +12,39 @@ export class FestivalWaiting extends PolymerElement {
       <style>
         :host {
           font-size: 40px;
+          text-align: center;
+        }
+
+        paper-button {
+          background-color: #ffb74d; /* material orange 300 */
+          color: #000;
+          border-radius: 8px;
         }
       </style>
-      Click to join
+      <p>The first set starts:<br />[[startTimeDescription]].</p>
+      <p><paper-button raised>Join</paper-button></p>
     `;
+  }
+
+  static get properties() {
+    return {
+      sets: {
+        type: Object
+      },
+      startTimeDescription: {
+        type: String,
+        computed: '_computeStartTimeDescription(sets)'
+      }
+    };
+  }
+
+  _computeStartTimeDescription(sets) {
+    const startTime = sets[0].start;
+    return moment(startTime).calendar(null, {
+      sameElse: function(now) {
+        return 'dddd, MMMM D' + (this.year() === now.year() ? '' : ', YYYY') + ' [at] h:mm A';
+      }
+    });
   }
 }
 
