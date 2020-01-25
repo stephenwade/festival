2be87d9e892f04edd04c5b6d4f0edb6520870966
Stephen Wade <stephen@stephenwade.me>
2020-01-25 13:13:22 -0500

    countdown to joining

--- src/festival-waiting/festival-waiting.js
+++ src/festival-waiting/festival-waiting.js
@@ -19,10 +19,25 @@ export class FestivalWaiting extends PolymerElement {
           background-color: #ffb74d; /* material orange 300 */
           color: #000;
           border-radius: 8px;
+          transition: min-width 0.25s ease-out;
+          min-width: 5em;
+        }
+
+        paper-button[disabled] {
+          background-color: rgba(255, 255, 255, 0.12);
+          min-width: 14em;
         }
       </style>
-      <p>The first set starts:<br />[[startTimeDescription]].</p>
-      <p><paper-button raised on-click="_handleJoinClicked">Join</paper-button></p>
+      <p>The first set starts:<br />[[_startTimeDescription]].</p>
+      <p>
+        <paper-button
+          raised
+          disabled="[[!_canJoin]]"
+          on-click="_handleJoinClicked"
+        >
+          [[_joinButtonText]]
+        </paper-button>
+      </p>
     `;
   }
 
@@ -31,20 +46,79 @@ export class FestivalWaiting extends PolymerElement {
       sets: {
         type: Object
       },
-      startTimeDescription: {
+      _now: {
+        type: moment
+      },
+      _startTime: {
+        type: moment,
+        computed: '_computeStartTime(sets)'
+      },
+      _startTimeDescription: {
         type: String,
-        computed: '_computeStartTimeDescription(sets)'
+        computed: '_computeStartTimeDescription(_startTime)'
+      },
+      _joinTime: {
+        type: moment,
+        computed: '_computeJoinTime(_startTime)'
+      },
+      _canJoin: {
+        type: Boolean,
+        computed: '_computeCanJoin(_joinTime, _now)'
+      },
+      _joinButtonText: {
+        type: String,
+        computed: '_computeJoinButtonText(_canJoin, _joinTime, _now)'
       }
     };
   }
 
-  _computeStartTimeDescription(sets) {
-    const startTime = sets[0].start;
-    return moment(startTime).calendar(null, {
+  connectedCallback() {
+    super.connectedCallback();
+    this._updateNow();
+    this._nowInterval = setInterval(() => {
+      this._updateNow();
+    }, 1000);
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    clearInterval(this._nowInterval);
+  }
+
+  _computeStartTime(sets) {
+    return moment(sets[0].start);
+  }
+
+  _computeStartTimeDescription(_startTime) {
+    return _startTime.calendar(null, {
       sameElse: function(now) {
-        return 'dddd, MMMM D' + (this.year() === now.year() ? '' : ', YYYY') + ' [at] h:mm A';
+        return (
+          'dddd, MMMM D' +
+          (this.year() === now.year() ? '' : ', YYYY') +
+          ' [at] h:mm A'
+        );
       }
     });
+  }
+
+  _computeJoinTime(_startTime) {
+    return _startTime.clone().subtract(1, 'hour');
+  }
+
+  _computeCanJoin(_joinTime, _now) {
+    if (_joinTime && _now) {
+      return _now.isAfter(_joinTime);
+    }
+    return false;
+  }
+
+  _computeJoinButtonText(_canJoin, _joinTime, _now) {
+    if (_joinTime && _now)
+      return 'Join' + (_canJoin ? '' : ' ' + _now.to(_joinTime));
+  }
+
+  _updateNow() {
+    this._now = moment();
   }
 
   _handleJoinClicked() {
