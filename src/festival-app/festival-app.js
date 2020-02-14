5ee9d547ac65e43cde2834f20481650441f363e5
Stephen Wade <stephen@stephenwade.me>
2020-02-14 17:24:48 -0500

    make the app the controller

--- src/festival-app/festival-app.js
+++ src/festival-app/festival-app.js
@@ -1,18 +1,13 @@
 import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
-import '../festival-controller/festival-controller.js';
 import '../festival-ui/festival-ui.js';
 
 export class FestivalApp extends PolymerElement {
   static get template() {
     return html`
-      <festival-controller
-        id="controller"
-        state="{{state}}"
-      ></festival-controller>
       <festival-ui
         id="ui"
         state="[[state]]"
-        on-action="handleAction"
+        on-action="_handleAction"
       ></festival-ui>
     `;
   }
@@ -23,8 +18,30 @@ export class FestivalApp extends PolymerElement {
     };
   }
 
-  handleAction(e) {
-    this.$.controller.action(e.detail);
+  connectedCallback() {
+    super.connectedCallback();
+
+    this._handleAction({ detail: { action: 'INIT' } });
+  }
+
+  _handleAction(e) {
+    const detail = e.detail;
+
+    switch (detail.action) {
+      case 'INIT':
+        this._initializeState();
+        break;
+
+      default:
+        throw new Error('Unknown action');
+    }
+  }
+
+  _initializeState() {
+    this.state = {
+      dataLoaded: false,
+      data: {}
+    };
   }
 }
 
