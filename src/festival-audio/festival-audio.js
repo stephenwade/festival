c7a10cb87bb1c55ac31808449d288fb6f96c5bae
Stephen Wade <stephen@stephenwade.me>
2020-02-22 12:32:27 -0500

    start in the middle of the set if needed

--- src/festival-audio/festival-audio.js
+++ src/festival-audio/festival-audio.js
@@ -49,33 +49,45 @@ export class FestivalAudio extends ActionMixin(PolymerElement) {
     this.audioContext.resume().then(() => {
       this.fireAction('AUDIO_CONTEXT_READY', { audioVisualizerData });
       this._status = 'NOT_STARTED';
-      this._queueStatusChange();
+      this._currentSetChanged(this.state.currentSet);
     });
   }
 
   _currentSetChanged(currentSet) {
-    if (this._status !== 'WAITING_FOR_AUDIO_CONTEXT' && currentSet) {
-      if (
-        !this._lastCurrentSet ||
-        currentSet.status !== this._lastCurrentSet.status ||
-        (currentSet.set &&
-          this._lastCurrentSet.set &&
-          currentSet.set.audio !== this._lastCurrentSet.set.audio)
-      ) {
-        this._queueStatusChange();
+    if (this._status === 'WAITING_FOR_AUDIO_CONTEXT') return;
+    if (!currentSet) return;
+
+    let change;
+
+    const firstRun = !this._lastCurrentSet;
+    if (firstRun) {
+      const set = this.state.currentSet.set;
+      change = {
+        status: this.state.currentSet.status,
+        src: set ? set.audio : undefined,
+        currentTime: this.state.currentSet.currentTimeInSet
+      };
+    } else {
+      const statusChanged = currentSet.status !== this._lastCurrentSet.status;
+      const set = this.state.currentSet.set;
+      const lastSet = this._lastCurrentSet.set;
+      const audioChanged = set && (!lastSet || set.audio !== lastSet.audio);
+      const loading = this._status === 'LOADING';
+      if (statusChanged || audioChanged || loading) {
+        change = {
+          status: this.state.currentSet.status,
+          src: audioChanged ? set.audio : undefined,
+          currentTime: this.state.currentSet.currentTimeInSet
+        };
       }
     }
+
+    if (change) this._queueStatusChange(change);
 
     this._lastCurrentSet = currentSet;
   }
 
-  _queueStatusChange() {
-    const change = {
-      status: this.state.currentSet.status,
-      src: this.state.currentSet.set.audio,
-      currentTime: this.state.currentSet.currentTimeInSet
-    };
-
+  _queueStatusChange(change) {
     switch (this._status) {
       case 'WAITING_FOR_AUDIO_CONTEXT':
         throw new Error(
@@ -83,6 +95,7 @@ export class FestivalAudio extends ActionMixin(PolymerElement) {
         );
 
       case 'NOT_STARTED':
+      case 'PRELOADING':
       case 'LOADING':
       case 'ENDED':
         this._performStatusChange(change);
@@ -100,19 +113,26 @@ export class FestivalAudio extends ActionMixin(PolymerElement) {
   _performStatusChange(change) {
     switch (change.status) {
       case 'WAITING':
-        if (this.$.audio.src !== change.src) this.$.audio.src = change.src;
-        this._status = 'LOADING';
+        if (change.src) this.$.audio.src = change.src;
+        this._status = 'PRELOADING';
         break;
 
       case 'IN_PROGRESS':
-        if (this.$.audio.src !== change.src) this.$.audio.src = change.src;
-        if (change.currentTime > 0) {
-          this.$.audio.play();
-          // not implemented yet
+        if (change.src) this.$.audio.src = change.src;
+        if (this._status === 'LOADING') {
+          if (change.currentTime >= this._audioStartTime) {
+            this.$.audio.play();
+            this._status = 'PLAYING';
+          }
+        } else if (change.currentTime > 0) {
+          this._status = 'LOADING';
+          // delay 2 seconds for audio to load
+          this._audioStartTime = change.currentTime + 2;
+          this.$.audio.src += `#t=${this._audioStartTime}`;
         } else {
           this.$.audio.play();
+          this._status = 'PLAYING';
         }
-        this._status = 'PLAYING';
         break;
 
       case 'ENDED':
