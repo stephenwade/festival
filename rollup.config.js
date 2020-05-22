802cc84a00b0decd37f0595b08d727a6e37a3731
Stephen Wade <stephen@stephenwade.me>
2020-05-22 11:57:32 -0400

    adjust Rollup config
    - open-wc/open-wc#1625

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+/*
 import merge from 'deepmerge';
 import { createSpaConfig } from '@open-wc/building-rollup';
 import copy from 'rollup-plugin-copy';
@@ -8,6 +9,57 @@ const baseConfig = createSpaConfig({
 });
 
 export default merge(baseConfig, {
+  input: './index.html',
+  plugins: [
+    copy({
+      targets: [{ src: 'images/*', dest: 'dist/images' }],
+    }),
+    replace({
+      __buildEnv__: 'production',
+    }),
+  ],
+});
+*/
+
+// until open-wc/open-wc#1625 and open-wc/open-wc#1666 are fixed, use this config
+// based on https://github.com/open-wc/open-wc/issues/1625#issuecomment-627605238
+
+import html from '@open-wc/rollup-plugin-html';
+import resolve from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import babel from 'rollup-plugin-babel';
+import {
+  babelConfigRollupGenerate,
+  createBabelConfigRollupBuild,
+} from '@open-wc/building-rollup/src/babel/babel-configs';
+
+babelConfigRollupGenerate.plugins = babelConfigRollupGenerate.plugins.slice(1);
+
+const c = {
+  preserveEntrySignatures: false,
+  treeshake: true,
+  output: {
+    format: 'es',
+    dir: 'dist',
+    entryFileNames: '[hash].js',
+    plugins: [babel.generated(babelConfigRollupGenerate)],
+  },
+  plugins: [
+    resolve(),
+    babel(createBabelConfigRollupBuild()),
+    terser({ output: { comments: false } }),
+    html({
+      minify: true,
+      inject: true,
+    }),
+  ],
+};
+
+import merge from 'deepmerge';
+import copy from 'rollup-plugin-copy';
+import replace from '@rollup/plugin-replace';
+
+export default merge(c, {
   input: './index.html',
   plugins: [
     copy({
