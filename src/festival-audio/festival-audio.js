e0c50d18d90d0662c56751ff137f3853bf0af032
Stephen Wade <stephen@stephenwade.me>
2020-05-27 21:29:07 -0400

    preload the next set before the current one ends

--- src/festival-audio/festival-audio.js
+++ src/festival-audio/festival-audio.js
@@ -6,22 +6,23 @@ export class FestivalAudio extends PolymerElement {
     super();
 
     this._changeQueue = [];
+
+    this._audioEvents = {
+      ended: this._handleAudioEnded.bind(this),
+      error: this._handleAudioError.bind(this),
+      loadedmetadata: this._handleAudioLoadedMetadata.bind(this),
+      pause: this._handleAudioPause.bind(this),
+      playing: this._handleAudioPlaying.bind(this),
+      stalled: this._handleAudioStalled.bind(this),
+      timeupdate: this._handleAudioTimeUpdate.bind(this),
+      waiting: this._handleAudioWaiting.bind(this),
+    };
   }
 
   static get template() {
     return html`
-      <audio
-        id="audio"
-        crossorigin="anonymous"
-        on-ended="_handleAudioEnded"
-        on-timeupdate="_handleAudioTimeUpdate"
-        on-error="_handleAudioError"
-        on-waiting="_handleAudioWaiting"
-        on-playing="_handleAudioPlaying"
-        on-pause="_handleAudioPause"
-        on-stalled="_handleAudioStalled"
-        on-loadedmetadata="_handleAudioLoadedMetadata"
-      ></audio>
+      <audio id="audio1" crossorigin="anonymous"></audio>
+      <audio id="audio2" crossorigin="anonymous"></audio>
     `;
   }
 
@@ -65,6 +66,33 @@ export class FestivalAudio extends PolymerElement {
     return ['_targetAudioStatusChanged(targetAudioStatus.*)'];
   }
 
+  connectedCallback() {
+    super.connectedCallback();
+
+    this._audioElements = [this.$.audio1, this.$.audio2];
+
+    this._audioElements.forEach((audio) => {
+      Object.keys(this._audioEvents).forEach((event) => {
+        const handler = this._audioEvents[event];
+        audio.addEventListener(event, handler);
+      });
+    });
+
+    this._activeAudio = this._audioElements[0];
+    this._inactiveAudio = this._audioElements[1];
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+
+    this._audioElements.forEach((audio) => {
+      Object.keys(this._audioEvents).forEach((event) => {
+        const handler = this._audioEvents[event];
+        audio.removeEventListener(event, handler);
+      });
+    });
+  }
+
   initialize() {
     if (this.targetShowStatus === 'ENDED') return;
 
@@ -78,42 +106,48 @@ export class FestivalAudio extends PolymerElement {
       }
     }
 
-    // Safari: activate the audio element by trying to play
-    this.$.audio.play().catch(() => {
-      // ignore errors
+    this._audioElements.forEach((audio) => {
+      // Safari: activate the audio element by trying to play
+      audio.play().catch(() => {
+        // ignore errors
+      });
+      // Firefox: if you don't pause after trying to play, it will start to play
+      // as soon as src is set
+      audio.pause();
     });
-    // Firefox: if you don't pause after trying to play, it will start to play
-    // as soon as src is set
-    this.$.audio.pause();
 
-    if (!this.audioContext) {
+    if (!this._audioContext) {
       this._handleAudioContextResumed();
     } else {
-      this.audioContext
+      this._audioContext
         .resume()
         .then(this._handleAudioContextResumed.bind(this));
     }
   }
 
   _setupAudioContext() {
-    this.audioContext = new AudioContext();
+    this._audioContext = new AudioContext();
 
-    const track = this.audioContext.createMediaElementSource(this.$.audio);
+    const tracks = this._audioElements.map((audio) =>
+      this._audioContext.createMediaElementSource(audio)
+    );
 
-    const analyserNode = this.audioContext.createAnalyser();
+    const analyserNode = this._audioContext.createAnalyser();
     analyserNode.fftSize = 1024;
     analyserNode.minDecibels = -85;
     analyserNode.smoothingTimeConstant = 0.7;
 
     const audioVisualizerData = new Uint8Array(analyserNode.frequencyBinCount);
 
-    // const gainNode = this.audioContext.createGain();
+    // const gainNode = this._audioContext.createGain();
     // gainNode.gain.value = 0.2;
 
-    track
-      .connect(analyserNode)
-      // .connect(gainNode)
-      .connect(this.audioContext.destination);
+    tracks.forEach((track) => {
+      track
+        .connect(analyserNode)
+        // .connect(gainNode)
+        .connect(this._audioContext.destination);
+    });
 
     this.getAudioVisualizerData = () => {
       analyserNode.getByteFrequencyData(audioVisualizerData);
@@ -176,20 +210,25 @@ export class FestivalAudio extends PolymerElement {
   }
 
   _performStatusChange(change) {
+    const setChanged = change.set !== this.audioStatus.set;
+    const nextSrcAlreadySet = Boolean(this._activeAudio.src);
+    const shouldChangeSrc = setChanged && !nextSrcAlreadySet;
+
     switch (change.status) {
       case 'WAITING_UNTIL_START':
-        if (change.set !== this.audioStatus.set)
-          this.$.audio.src = change.set.audio;
+        if (shouldChangeSrc) this._activeAudio.src = change.set.audio;
+
         this.audioStatus = {
           set: change.set,
           status: 'WAITING_UNTIL_START',
           secondsUntilSet: change.secondsUntilSet,
+          nextSet: change.nextSet,
         };
         break;
 
       case 'PLAYING':
-        if (change.set !== this.audioStatus.set)
-          this.$.audio.src = change.set.audio;
+        if (shouldChangeSrc) this._activeAudio.src = change.set.audio;
+
         if (change.currentTime > 0) {
           // delay 2 seconds for audio to load
           const delayingUntil = change.currentTime + 2;
@@ -197,14 +236,16 @@ export class FestivalAudio extends PolymerElement {
             set: change.set,
             status: 'DELAYING_FOR_INITIAL_SYNC',
             delayingUntil,
+            nextSet: change.nextSet,
           };
-          this.$.audio.src += `#t=${delayingUntil}`;
+          this._activeAudio.src += `#t=${delayingUntil}`;
         } else {
-          this.$.audio.play().catch(this._handleAudioError.bind(this));
+          this._activeAudio.play().catch(this._handleAudioError.bind(this));
           this.audioStatus = {
             set: change.set,
             status: 'PLAYING',
             currentTime: 0,
+            nextSet: change.nextSet,
           };
         }
         break;
@@ -233,7 +274,7 @@ export class FestivalAudio extends PolymerElement {
 
       case 'DELAYING_FOR_INITIAL_SYNC':
         if (change.currentTime >= this.audioStatus.delayingUntil) {
-          this.$.audio.play();
+          this._activeAudio.play();
           this.audioStatus = {
             set: change.set,
             status: 'PLAYING',
@@ -251,7 +292,7 @@ export class FestivalAudio extends PolymerElement {
   }
 
   _getDelay(change) {
-    let delay = change.currentTime - this.$.audio.currentTime;
+    let delay = change.currentTime - this._activeAudio.currentTime;
     if (change.set !== this.audioStatus.set) {
       const setDifference = change.set.startMoment.diff(
         this.audioStatus.set.startMoment,
@@ -264,7 +305,20 @@ export class FestivalAudio extends PolymerElement {
     return delay;
   }
 
-  _handleAudioEnded() {
+  _clearAndSwitchActiveAudio() {
+    this._activeAudio.removeAttribute('src');
+
+    // prettier-ignore
+    // swap _activeAudio and _inactiveAudio
+    [this._activeAudio, this._inactiveAudio] =
+      [this._inactiveAudio, this._activeAudio];
+  }
+
+  _handleAudioEnded(e) {
+    if (e.target !== this._activeAudio) return;
+
+    this._clearAndSwitchActiveAudio();
+
     delete this.audioStatus.set;
     delete this.audioStatus.delay;
     this.set('audioStatus.status', 'WAITING_UNTIL_START');
@@ -278,10 +332,22 @@ export class FestivalAudio extends PolymerElement {
     this.audioPaused = false;
   }
 
-  _handleAudioTimeUpdate() {
+  _handleAudioTimeUpdate(e) {
+    if (e.target !== this._activeAudio) return;
+
     if (this.audioStatus.status === 'PLAYING') {
-      const currentTime = this.$.audio.currentTime;
+      const currentTime = this._activeAudio.currentTime;
       this.set('audioStatus.currentTime', currentTime);
+
+      const nextSrcAlreadySet = Boolean(this._inactiveAudio.src);
+      const nextSetAvailable = Boolean(this.audioStatus.nextSet);
+      const lessThanOneMinuteLeft =
+        this.audioStatus.set.length - this.audioStatus.currentTime <= 60;
+      const shouldPreloadNextSet =
+        !nextSrcAlreadySet && nextSetAvailable && lessThanOneMinuteLeft;
+      if (shouldPreloadNextSet) {
+        this._inactiveAudio.src = this.audioStatus.nextSet.audio;
+      }
     }
   }
 
@@ -295,7 +361,9 @@ export class FestivalAudio extends PolymerElement {
     this._error = true;
   }
 
-  _handleAudioWaiting() {
+  _handleAudioWaiting(e) {
+    if (e.target !== this._activeAudio) return;
+
     this.audioWaiting = true;
 
     this._stalledTimeout = setTimeout(
@@ -304,7 +372,9 @@ export class FestivalAudio extends PolymerElement {
     );
   }
 
-  _handleAudioPlaying() {
+  _handleAudioPlaying(e) {
+    if (e.target !== this._activeAudio) return;
+
     this.audioWaiting = false;
 
     this.audioStalled = false;
@@ -313,25 +383,34 @@ export class FestivalAudio extends PolymerElement {
     this.audioPaused = false;
   }
 
-  _handleAudioPause() {
+  _handleAudioPause(e) {
+    if (e.target !== this._activeAudio) return;
+
     this.audioPaused = true;
   }
 
-  _handleAudioStalled() {
+  _handleAudioStalled(e) {
+    if (e.target !== this._activeAudio) return;
+
     // Safari: only listen to stalled event if audio is waiting
     if (!this.audioWaiting) return;
 
     this.audioStalled = true;
   }
 
-  _handleAudioLoadedMetadata() {
+  _handleAudioLoadedMetadata(e) {
+    const set =
+      e.target === this._activeAudio
+        ? this.audioStatus.set
+        : this.audioStatus.nextSet;
+
     this.dispatchEvent(
       new CustomEvent('loadedmetadata', {
         bubbles: true,
         composed: true,
         detail: {
-          set: this.audioStatus.set,
-          duration: this.$.audio.duration,
+          set,
+          duration: e.target.duration,
         },
       })
     );
