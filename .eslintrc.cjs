5f65fef5dfc821c3b583f4c419f21af229efd7fc
Stephen Wade <stephen@stephenwade.me>
2021-03-27 22:07:03 -0400

    override rules for HTML test files
    ref: open-wc/open-wc#2107

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,19 @@
 module.exports = {
   extends: ['@open-wc/eslint-config', 'eslint-config-prettier'],
+
+  // TODO: remove when https://github.com/open-wc/open-wc/pull/2107 is merged
+  overrides: [
+    {
+      files: [
+        '**/test/**/*.{html,js,mjs,ts}',
+        '**/demo/**/*.{html,js,mjs,ts}',
+        '**/stories/**/*.{html,js,mjs,ts}',
+      ],
+      rules: {
+        'no-console': 'off',
+        'no-unused-expressions': 'off',
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
+  ],
 };
