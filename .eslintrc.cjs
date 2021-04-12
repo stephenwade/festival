7f8e31c8cc284cabb38e251e84795d510be40b7f
Stephen Wade <stephen@stephenwade.me>
2021-04-12 00:31:56 -0400

    add two rules to ESLint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,11 @@
 module.exports = {
   extends: ['@open-wc/eslint-config', 'eslint-config-prettier'],
 
+  rules: {
+    'require-unicode-regexp': 'error',
+    'no-undef': ['error', { typeof: true }],
+  },
+
   // TODO: remove when https://github.com/open-wc/open-wc/pull/2107 is merged
   overrides: [
     {
