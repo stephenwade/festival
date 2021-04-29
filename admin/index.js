05a192ad4bd2939a15761fb591faa87edc983079
Stephen Wade <stephen@stephenwade.me>
2021-04-29 19:41:08 -0400

    add Rollup and UI to admin site

--- admin/index.js
+++ admin/index.js
@@ -1,5 +1,6 @@
 import Koa from 'koa';
 import send from 'koa-send';
+import serve from 'koa-static';
 import Router from '@koa/router';
 
 const PORT = 8000;
@@ -7,11 +8,13 @@ const PORT = 8000;
 const app = new Koa();
 const router = new Router();
 
-router.get('/sets.json', async (ctx) => {
+router.get('/api/sets', async (ctx) => {
   await send(ctx, 'sets.json');
 });
 
 app.use(router.routes()).use(router.allowedMethods());
+
+app.use(serve('ui/dist'));
 
 app.listen(8000);
 console.log(`Listening on port ${PORT}`);
