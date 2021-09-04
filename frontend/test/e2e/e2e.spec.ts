93a335aeed4126f03d1cbda309606a6101018653
Stephen Wade <stephen@stephenwade.me>
2021-09-03 22:28:07 -0400

    f

--- frontend/test/e2e/e2e.spec.ts
+++ frontend/test/e2e/e2e.spec.ts
@@ -7,9 +7,9 @@ test('e2e', async ({ page }) => {
 
   await page.click('a:has-text("Listen Live")');
 
-  await page.waitForSelector('#current-time >> text="0:01"')
+  await page.waitForSelector('#current-time >> text="0:01"');
   expect((await page.innerText('#next-up')).toUpperCase()).to.equal('NEXT UP');
 
-  await page.waitForSelector('#current-time >> text="0:05"')
+  await page.waitForSelector('#current-time >> text="0:05"');
   expect((await page.innerText('#artist')).toUpperCase()).to.equal('ARTIST 1');
 });
