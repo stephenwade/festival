5eda9b93e997dd683197806c7ee4be68648e36c6
Stephen Wade <stephen@stephenwade.me>
2021-09-03 22:20:47 -0400

    Fix E2E timing incconsistency

--- frontend/test/e2e/e2e.spec.ts
+++ frontend/test/e2e/e2e.spec.ts
@@ -6,20 +6,10 @@ test('e2e', async ({ page }) => {
   await page.waitForTimeout(500);
 
   await page.click('a:has-text("Listen Live")');
-  await page.waitForTimeout(500);
 
-  expect(await page.innerText('#next-up')).to.equal('NEXT UP');
-  // Timers are weird in CI
-  expect(await page.innerText('#current-time')).to.satisfy(
-    (time) => time === '0:03' || time === '0:02' || time === '0:01'
-  );
+  await page.waitForSelector('#current-time >> text="0:01"')
+  expect((await page.innerText('#next-up')).toUpperCase()).to.equal('NEXT UP');
 
-  await page.waitForTimeout(8.5 * 1000);
-
-  expect(await page.innerText('#artist')).to.equal('ARTIST 1');
-  // Timers are weird in CI
-  expect(await page.innerText('#current-time')).to.satisfy(
-    (time) =>
-      time === '0:05' || time === '0:06' || time === '0:07' || time === '0:08'
-  );
+  await page.waitForSelector('#current-time >> text="0:05"')
+  expect((await page.innerText('#artist')).toUpperCase()).to.equal('ARTIST 1');
 });
