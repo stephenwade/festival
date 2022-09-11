020c9bf5521876afba663108479f6fa682c02bcd
Stephen Wade <stephen@stephenwade.me>
2022-09-11 19:38:42 -0400

    Spinner component

--- app/components/ShowPlaying/ShowPlaying.tsx
+++ app/components/ShowPlaying/ShowPlaying.tsx
@@ -2,25 +2,22 @@ import type { LinksFunction } from '@remix-run/node';
 import type { FC } from 'react';
 import { useState } from 'react';
 
+import { links as spinnerLinks, Spinner } from '~/components/Spinner';
+import { links as volumeLinks, VolumeFab } from '~/components/VolumeFab';
 import stylesUrl from '~/styles/show-playing.css';
 
-import { links as volumeLinks, VolumeFab } from '../VolumeFab';
-
 export const links: LinksFunction = () => [
+  ...spinnerLinks(),
   ...volumeLinks(),
   { rel: 'stylesheet', href: stylesUrl },
 ];
-
-function LoadingSpinner() {
-  return null;
-}
 
 type Props = {
   onShowEnded: () => void;
 };
 
 export const ShowPlaying: FC<Props> = ({ onShowEnded }) => {
-  const [showSpinner] = useState(false);
+  const [showSpinner, setShowSpinner] = useState(true);
   const [waitingUntilStart] = useState(true);
   const volume = 100;
   const set = { artist: "'Etikit" };
@@ -30,11 +27,18 @@ export const ShowPlaying: FC<Props> = ({ onShowEnded }) => {
   return (
     <div className="playing-container full-page">
       <canvas className="canvas full-page"></canvas>
-      <div className="current-time" onClick={onShowEnded}>
-        {showSpinner ? <LoadingSpinner /> : currentTime}
+      <div
+        className="current-time"
+        onClick={() => {
+          setShowSpinner((showSpinner) => !showSpinner);
+        }}
+      >
+        {showSpinner ? <Spinner /> : currentTime}
       </div>
       {waitingUntilStart ? <div className="next-up">Next up</div> : null}
-      <div className="artist">{set.artist}</div>
+      <div className="artist" onClick={onShowEnded}>
+        {set.artist}
+      </div>
       <VolumeFab
         volume={volume}
         // @volumeinput="${this._handleVolumeInput}"
