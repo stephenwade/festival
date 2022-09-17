ac14434da31ef3fa72618a3d2b2dff8fafb1260b
Stephen Wade <stephen@stephenwade.me>
2022-09-16 22:00:05 -0400

    useClock hook

---
+++ app/hooks/useClock.ts
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from 'react';
+
+type UseClockProps = {
+  onTick: () => void;
+  enabled?: boolean;
+};
+
+export const useClock = ({ onTick, enabled = true }: UseClockProps): void => {
+  const getCurrentSecond = () => Math.floor(Date.now() / 1000);
+
+  const lastSecond = useRef(getCurrentSecond());
+
+  useEffect(() => {
+    if (enabled) {
+      const interval = setInterval(() => {
+        const now = getCurrentSecond();
+        if (lastSecond.current !== now) {
+          lastSecond.current = now;
+          onTick();
+        }
+      }, 200);
+
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [enabled, onTick]);
+};
