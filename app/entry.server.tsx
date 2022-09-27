a0a704d9c85ee6c81225bfedf6edf8d4a87de54c
Stephen Wade <stephen@stephenwade.me>
2022-09-26 22:55:28 -0400

    Remove streaming from server entry point

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,12 +1,6 @@
-import { PassThrough } from 'node:stream';
-
-import type { EntryContext, Headers } from '@remix-run/node';
-import { Response } from '@remix-run/node';
+import type { EntryContext } from '@remix-run/node';
 import { RemixServer } from '@remix-run/react';
-import isbot from 'isbot';
-import { renderToPipeableStream } from 'react-dom/server';
-
-const ABORT_DELAY = 5000;
+import { renderToString } from 'react-dom/server';
 
 export default function handleRequest(
   request: Request,
@@ -14,39 +8,14 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  const callbackName = isbot(request.headers.get('user-agent'))
-    ? 'onAllReady'
-    : 'onShellReady';
+  const markup = renderToString(
+    <RemixServer context={remixContext} url={request.url} />
+  );
 
-  return new Promise((resolve, reject) => {
-    let didError = false;
+  responseHeaders.set('content-type', 'text/html');
 
-    // eslint-disable-next-line @typescript-eslint/unbound-method
-    const { pipe, abort } = renderToPipeableStream(
-      <RemixServer context={remixContext} url={request.url} />,
-      {
-        [callbackName]() {
-          const body = new PassThrough();
-
-          responseHeaders.set('Content-Type', 'text/html');
-
-          resolve(
-            new Response(body, {
-              status: didError ? 500 : responseStatusCode,
-              headers: responseHeaders,
-            })
-          );
-          pipe(body);
-        },
-        onShellError(err) {
-          reject(err);
-        },
-        onError(error) {
-          didError = true;
-          console.error(error);
-        },
-      }
-    );
-    setTimeout(abort, ABORT_DELAY);
+  return new Response('<!DOCTYPE html>' + markup, {
+    status: responseStatusCode,
+    headers: responseHeaders,
   });
 }
