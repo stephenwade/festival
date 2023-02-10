474eded9eaf8280b91850a3ae9b61a84859113a6
Stephen Wade <stephen@stephenwade.me>
2023-02-09 21:54:52 -0500

    Update Stylelint to v15 (#872)

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,20 +1,21 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-const postcssCssInJs = require('@stylelint/postcss-css-in-js')();
-// ^^ requires postcss-syntax to be installed
-
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-prettier',
-    'stylelint-config-hudochenkov/order',
-  ],
+  extends: ['stylelint-config-standard', 'stylelint-config-hudochenkov/order'],
   rules: {
     'selector-type-no-unknown': [true, { ignore: ['custom-elements'] }],
 
     // disabled because it forbids the :host selector
     'selector-pseudo-element-colon-notation': null,
 
-    'declaration-block-trailing-semicolon': 'always',
+    'declaration-property-value-no-unknown': true,
   },
-  customSyntax: postcssCssInJs,
+  overrides: [
+    {
+      files: ['**/*.js'],
+      customSyntax: 'postcss-lit',
+    },
+    {
+      files: ['**/*.html'],
+      customSyntax: 'postcss-html',
+    },
+  ],
 };
