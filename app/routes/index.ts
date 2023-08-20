e6873a72eaeeb763e8cff39a2a8c67012f566856
Stephen Wade <stephen@stephenwade.me>
2023-08-20 19:08:24 -0400

    Redirect to first show (#1097)

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,6 +1,15 @@
 import type { LoaderFunction } from '@remix-run/node';
 import { redirect } from '@remix-run/node';
 
-export const loader = (() => {
-  return redirect('/my-show');
+import { db } from '~/db/db.server';
+
+const notFound = () => new Response('Not Found', { status: 404 });
+
+export const loader = (async () => {
+  const show = await db.show.findFirst({
+    orderBy: { startDate: 'asc' },
+  });
+  if (!show) throw notFound();
+
+  return redirect(`/${show.id}`);
 }) satisfies LoaderFunction;
