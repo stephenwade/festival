2ff85a33aa905d39124d477e7b61c4fa4537855e
Stephen Wade <stephen@stephenwade.me>
2023-10-09 23:15:24 -0400

    Enable v2_dev

--- remix.config.js
+++ remix.config.js
@@ -6,6 +6,7 @@ module.exports = {
   serverDependenciesToBundle: ['use-local-storage-state'],
   serverModuleFormat: 'cjs',
   future: {
+    v2_dev: true,
     v2_errorBoundary: true,
     v2_headers: true,
     v2_meta: true,
