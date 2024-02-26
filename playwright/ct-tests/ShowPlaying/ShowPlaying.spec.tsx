49a48b17cb2a8905c6accf1ad1ab034205f9f44a
Stephen Wade <stephen@stephenwade.me>
2024-02-25 23:19:39 -0500

    Continue ShowPlaying tests

--- playwright/ct-tests/ShowPlaying/ShowPlaying.spec.tsx
+++ playwright/ct-tests/ShowPlaying/ShowPlaying.spec.tsx
@@ -21,6 +21,21 @@ function makeShowInfoWaitingUntilStart(secondsUntilSet: number) {
   } satisfies ShowInfo;
 }
 
+function makeShowInfoPlaying(currentTime: number) {
+  return {
+    status: 'PLAYING',
+    currentTime,
+    currentSet: {
+      id: '69859842-c6ac-3aa4-8f0d-358ff7758b9e',
+      audioUrl: 'sample/dana.mp3',
+      artist: 'Dana',
+      start: addSeconds(Date.now(), -currentTime),
+      end: addSeconds(Date.now(), 30 - currentTime),
+      duration: 30,
+    },
+  } satisfies ShowInfo;
+}
+
 function showPlayingTemplate(
   props: Partial<ShowPlayingProps> & Pick<ShowPlayingProps, 'showInfo'>,
 ) {
@@ -57,7 +72,7 @@ test.describe('while waiting for start', () => {
     );
 
     await expect(component).toContainText('2:45');
-    await expect(component).not.toContainText('0:02:45');
+    await expect(component).not.toContainText('02:45');
   });
 
   test('includes hours if there is an hour or more until the set', async ({
@@ -71,5 +86,43 @@ test.describe('while waiting for start', () => {
 
     await expect(component).toContainText('1:02:45');
     await expect(component).not.toContainText('01:02:45');
+  });
+});
+
+test.describe('while playing', () => {
+  const showInfo = makeShowInfoPlaying(15);
+
+  test('does not include the words "NEXT UP"', async ({ mount }) => {
+    const component = await mount(showPlayingTemplate({ showInfo }));
+
+    await expect(component).not.toContainText('NEXT UP');
+  });
+
+  test('includes the artist name', async ({ mount }) => {
+    const component = await mount(showPlayingTemplate({ showInfo }));
+
+    await expect(component).toContainText('Dana');
+  });
+
+  test('includes minutes and seconds', async ({ mount }) => {
+    const component = await mount(
+      showPlayingTemplate({ showInfo: makeShowInfoPlaying(295) }),
+    );
+
+    await expect(component).toContainText('4:55');
+    await expect(component).not.toContainText('04:55');
+  });
+
+  test('includes hours if the current time is more than an hour', async ({
+    mount,
+  }) => {
+    const component = await mount(
+      showPlayingTemplate({
+        showInfo: makeShowInfoPlaying(295 + 3600),
+      }),
+    );
+
+    await expect(component).toContainText('1:04:55');
+    await expect(component).not.toContainText('01:04:55');
   });
 });
