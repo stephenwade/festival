eb7899414759351807bfac1788abb0b7d004f955
Stephen Wade <stephen@stephenwade.me>
2024-04-20 17:24:10 -0400

    Skip failing tests in Webkit

--- playwright/ct-tests/AudioController/AudioController.spec.tsx
+++ playwright/ct-tests/AudioController/AudioController.spec.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable playwright/no-wait-for-timeout */
 
+import { platform } from 'node:process';
+
 import { expect as baseExpect, test } from '@playwright/experimental-ct-react';
 import type { Locator } from '@playwright/test';
 
@@ -106,6 +108,7 @@ function commonTests({ forceSkipAudioContext = false }) {
     test('does not play audio until the show starts', async ({
       mount,
       page,
+      browserName,
     }) => {
       const component = await mount(
         <AudioControllerTest
@@ -117,6 +120,12 @@ function commonTests({ forceSkipAudioContext = false }) {
 
       await page.waitForTimeout(500);
       await expectAudioIsNotPlaying(component, '1.5 seconds before the show');
+      test.fail(
+        browserName === 'webkit' &&
+          platform !== 'darwin' &&
+          forceSkipAudioContext,
+        "Playing audio doesn't work on Webkit outside of macOS",
+      );
       await page.waitForTimeout(3000);
       await expectAudioIsPlaying(component, 'during the show');
       await expectAudioCurrentTimeToAlmostEqual(component, 1);
