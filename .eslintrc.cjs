a96f6f513844a534bd24dcfa8464eb9e572ab024
Stephen Wade <stephen@stephenwade.me>
2024-05-24 20:15:43 -0400

    Add comments about ESLint v9 to .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,17 +4,32 @@ module.exports = {
     project: true,
   },
 
-  plugins: ['import', 'no-only-tests', 'simple-import-sort', 'unused-imports'],
+  // Comments link to blockers for flat config
+  plugins: [
+    // https://github.com/import-js/eslint-plugin-import/pull/2873
+    // https://github.com/import-js/eslint-plugin-import/issues/2948
+    // https://github.com/un-ts/eslint-plugin-import-x/issues/29
+    'import',
+    // https://github.com/levibuzolic/eslint-plugin-no-only-tests/issues/43
+    'no-only-tests',
+    'simple-import-sort',
+    'unused-imports',
+  ],
 
   extends: [
     'eslint:recommended',
 
+    // https://github.com/jsx-eslint/eslint-plugin-react/issues/3699
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
+    // https://github.com/facebook/react/issues/28313
     'plugin:react-hooks/recommended',
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/pull/891
+    // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/issues/978
     'plugin:jsx-a11y/recommended',
     'plugin:playwright/recommended',
 
+    // https://github.com/typescript-eslint/typescript-eslint/milestone/9
     'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:@typescript-eslint/stylistic-type-checked',
 
