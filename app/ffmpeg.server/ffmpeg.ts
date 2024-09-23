9645b6e158eeeddcd8c86c029d2f0f475dd9b0fa
Stephen Wade <stephen@stephenwade.me>
2024-09-22 22:47:21 -0400

    Refactor Zod number schemas into a separate file

--- app/ffmpeg.server/ffmpeg.ts
+++ app/ffmpeg.server/ffmpeg.ts
@@ -2,10 +2,7 @@ import { spawn } from 'node:child_process';
 
 import { z } from 'zod';
 
-const integerString = z
-  .string()
-  .regex(/^[0-9]+$/u)
-  .transform(Number);
+import { integerString } from '~/utils/zod-number-schemas';
 
 /**
  * Parses a string containing progress output in `key=value` format from FFmpeg.
