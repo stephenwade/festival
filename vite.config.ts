e90332b88d2f24b05e3229de4cd2433d2cd56258
Stephen Wade <stephen@stephenwade.me>
2025-01-14 20:36:53 -0500

    Enable v3_lazyRouteDiscovery

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     remix({
       future: {
         v3_fetcherPersist: true,
+        v3_lazyRouteDiscovery: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
       },
