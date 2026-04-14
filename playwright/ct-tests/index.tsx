a6ff80ed848aaf66b9bcae44fbc0555dcc008a1e
Stephen Wade <stephen@stephenwade.me>
2026-04-14 11:47:11 -0400

    Put back Helmet change

--- playwright/ct-tests/index.tsx
+++ playwright/ct-tests/index.tsx
@@ -3,6 +3,8 @@
 import { beforeMount } from '@playwright/experimental-ct-react/hooks';
 import { HelmetProvider } from 'react-helmet-async';
 
+HelmetProvider.canUseDOM = false;
+
 beforeMount(({ App }) => {
   return Promise.resolve(
     <HelmetProvider>
